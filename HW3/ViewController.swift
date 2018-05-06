5be2df35706523f96967b44cdde9ca4f6fd9a3f9
Alexandr <alex@MacBook-Pro-Alexandr.local>
2018-05-06 18:53:43 +0300

    made easy string tasks

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -10,14 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        print("String Easy. Task 1")
+        print("My name has \(lettersCount(myNameIs: "Alex")) characters")
+        print("==========================================")
+
+
+        print("String Easy. Task 2")
+        user(name: "Alex", lastName: "Bes")
+        print("==========================================")
+
+        print("String Easy. Task 3")
+        let nameAndMidleName = "Александр КонстантиновИч"
+        manOrWoman(name: nameAndMidleName)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+//      Дополнительное задание
+
+    //    Block 1. String Easy. Task 1
+    //    Создать строку со своим именем. Написать метод который получает это имя и возвращает кол-во символов в строке с вашим именем. Возвращенное значение показать в консоль
+
+
+    let myNameIs = ""
+
+    func lettersCount(myNameIs: String) -> Int {
+
+        return myNameIs.count
+    }
+
+
+//      String Easy. Task 2
+//      Написать метод который принимает 2 слова, например имя и фамилию, возвращает юзернейм который имеет буквы нижнего регистра и разделяет имя и фамилию символом “_”
+
+
+    func user(name: String, lastName: String) {
+print(name.lowercased(), lastName.lowercased(), separator: "_")
+    }
+
+//      String Easy. Task 3
+//      Создать строку с именем и своим отчеством. Создать метод который проверит его на окончание “ич/на”. Выводит “М” или “Ж” в зависимости от окончания. Также учитывать регистр букв, то есть ИЧ или Ич или На и тд.
+//      Подсказка: в классе написан метод который позволяет проверить на суффикс или префикс, найдите и используйте его
+
+    func manOrWoman(name: String) {
+        let someString = name.lowercased()
+        if someString.hasSuffix("ич") {
+            print("Man")
+        } else if someString.hasSuffix("на") {
+            print("Woman")
+        } else {
+            print("It hasn't middle name")
+        }
     }
 
 
